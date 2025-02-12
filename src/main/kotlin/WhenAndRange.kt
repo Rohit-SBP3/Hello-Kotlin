c5828a4ab5b79d56d27859d000d4acbae6955507
Rohit Singh <rohitrajput95547@gmail.com>
2025-02-12 20:01:13 +0530

    When and Range

---
+++ src/main/kotlin/WhenAndRange.kt
@@ -0,0 +1,36 @@
+fun main(){
+
+    val n = 5
+    println(n in 1..5) // UpperBound is Included
+    println(n in 1 until 5) // UpperBound is not Included
+
+    val animal = "Dog"
+    if(animal == "Cat") println("Animal is Cat")
+    else if(animal == "Lion") println("Animal is Lion")
+    else if(animal == "Dog") println("Animal is Dog")
+    else println("Animal is Dead!!!!")
+
+    var ani = "Whale"
+    when(ani){
+        "Horse" ->  println("Animal is Horse")
+        "Dog" ->  println("Animal is Dog")
+        "Cat" ->  println("Animal is Cat")
+        else -> println("Animal is Dead")
+    }
+
+    ani = when(ani){
+        "Horse" ->  "Animal is Horse"
+        "Dog" ->  "Animal is Dog"
+        "Cat" ->  "Animal is Cat"
+        else -> "Animal is Dead"
+    }
+
+    val num = 13
+    val res = when(num){
+        12 -> "Twelve"
+        in 13..19 -> "teen"
+        else -> "I don't Know"
+    }
+    println(res)
+    println(ani)
+}
