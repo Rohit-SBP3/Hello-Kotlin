3c41195a07013cbaa651a2ad6c56e23e06199527
Rohit Singh <rohitrajput95547@gmail.com>
2025-02-12 19:04:57 +0530

    Basics of Kotlin

---
+++ src/main/kotlin/Operators.kt
@@ -0,0 +1,17 @@
+fun main(){
+
+
+    // Simple Arithmetic Operators( +, -, /, *, %)
+    // Relational Operators( >, <, <=, >=, !=, ==)
+    // Increment and Decrement( i++, ++i, i--, --i) (Post and Pre)
+    // Logical Operators( &&, ||, !)
+    // NOTE: Short Circuiting (checks only one condition) &&(1st false then out) ||(first true then out)
+
+    var a = 10;
+    var b = 3;
+
+    println(a/b)
+    println(a.toFloat()/b)
+    println(a++ + ++a - b-- - --b)
+    print(a == b)
+}
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,30 @@
+fun main(){
+
+    // Kotlin Docs and Kotlin Playground
+    // Kotlin Koans
+
+    // Var and Val
+    // Data Types: Integer(Byte, Short, Int, Long),
+    // Floating Point(Double, Float)
+    // Boolean(True,False)
+    // Character(Char, String)
+
+    // Type Checking: Compile Time
+    val bt: Byte = 10
+    val sh: Short = 120
+    var five: Int = 5
+    var lng: Long = 1039L
+
+    var temperature: Double = 12.5
+    var score: Float = 120.34F
+
+    var areYouGay: Boolean = false
+
+    var alphabet: Char = 'A'
+    var st: String  = "Rohit Singh"
+
+    println(bt+sh+five+lng+temperature+score)
+    println(areYouGay)
+    print("$alphabet $st")
+
+}
