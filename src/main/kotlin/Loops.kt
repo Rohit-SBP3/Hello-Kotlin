181a41ee9486c9679aa7412a945fd3443e522bf4
Rohit Singh <rohitrajput95547@gmail.com>
2025-02-14 19:44:45 +0530

    Loops

--- src/main/kotlin/Loops.kt
+++ src/main/kotlin/Loops.kt
@@ -1,10 +1,11 @@
 // While and Do-While.
 
-fun main(){
+fun main() {
+
+    // While and Do-While....................................................
 
     var num = 10
     var ind = 1
-
     while(num-- > 0){
         println(num)
     }
@@ -16,10 +17,43 @@ fun main(){
     }
     print("Index outside $ind")
 
-    //Do While.................................
+
+    // Do While.............................................................
+
     var i = 10
     val j = 20
     do{
         println("Sum of $i and $j is: " + (i+j))
     }while(i-- > 0)
+
+
+    // For Loop, Ranges and String Templating...............................
+
+    // Include UpperBound
+    for(a in 1..10) print("$a ")
+
+    println()
+
+    // Exclude UpperBound
+    for(a in 1 until 10) print("$a ")
+
+    println()
+
+    for(a in 10 downTo 1) print("$a ")
+
+    println()
+
+    table(23)
+
+}
+
+fun table(num: Int){
+    for(i in 1..10){
+        val pro = num * i
+        println("$num * $i = $pro")
+    }
+
+    for(i in 1..10){
+        println(num.toString() + " x " + i + " = " + (num * i))
+    }
 }
