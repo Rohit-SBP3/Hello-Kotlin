9fabccaaadeb35f924fd82b3720e9735e9dd7d6f
Rohit Singh <rohitrajput95547@gmail.com>
2025-02-19 19:37:34 +0530

    Functions

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,7 +1,21 @@
 fun main(){
-    print(add(10,20))
+    println(add(10,20))
+    println(isEven(13))
+    isEvenOrOdd(21)
 }
 
+// Return type Int
 fun add(a: Int, b: Int): Int {
     return a+b
+}
+
+//Return type Unit
+fun isEvenOrOdd(a: Int):Unit{
+    if(a % 2 == 0) print("Even")
+    else print("Odd")
+}
+
+// Return type Boolean
+fun isEven(b: Int):Boolean{
+    return b % 2 == 0
 }
