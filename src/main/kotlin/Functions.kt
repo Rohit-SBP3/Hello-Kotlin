7778639600b6e867fe3ffa65af9026f17ce0396c
Rohit Singh <rohitrajput95547@gmail.com>
2025-02-19 19:52:11 +0530

    Inline Function and Default Param

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -2,20 +2,35 @@ fun main(){
     println(add(10,20))
     println(isEven(13))
     isEvenOrOdd(21)
+    println(addThree(12,32,34))
+    sayHelloToNum(5)
+
+    // The value we define in function is known as parameter,
+    // The value we pass is known as argument.
+
+    //Note: var on function parameter is not allowed.
 }
 
 // Return type Int
-fun add(a: Int, b: Int): Int {
+fun add(var a: Int, b: Int): Int {
     return a+b
 }
 
 //Return type Unit
-fun isEvenOrOdd(a: Int):Unit{
-    if(a % 2 == 0) print("Even")
-    else print("Odd")
+fun isEvenOrOdd(a: Int) {
+    if(a % 2 == 0) println("Even")
+    else println("Odd")
 }
 
 // Return type Boolean
 fun isEven(b: Int):Boolean{
     return b % 2 == 0
+}
+
+// Inline Function( if return type is same no need to mention
+fun addThree(a: Int, b: Int, c: Int) = a + b + c
+
+// Default value to Pram
+fun sayHelloToNum(num: Int = 10){
+    for (i in 1..num) println("Hello $i")
 }
