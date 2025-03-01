04d42318611d7b0ef4559a986691fc825e1ba6d0
Rohit Singh <rohitrajput95547@gmail.com>
2025-03-01 19:46:32 +0530

    Array

---
+++ src/main/kotlin/Array.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    val vowels: Array<Char> = arrayOf('a','e','i','o','u')
+
+    val num: Array<Int> = arrayOf(1,2,3,4,5,6,7,8,9,10)
+
+    val bigNum = arrayOf<Int>(102030,105739)
+
+    for (c in vowels) println(c)
+
+    for((i , e) in num.withIndex()) println("$i -> $e")
+
+    println(num[5])
+    num.set(5,402)
+    println(num.get(5))
+
+
+}
