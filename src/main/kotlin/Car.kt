15de8ad4be89a6e8390e06dd1d72099f285724e1
Rohit Singh <rohitrajput95547@gmail.com>
2025-03-07 12:42:22 +0530

    Class Properties & Methods

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,5 +1,19 @@
 class Car(
+    // Properties of a class
     val name: String,
     val type: String,
-    var kmRange: Int,
-)
+    var kmRan: Int,
+){
+
+    // methods of a class
+
+    fun driveCar(km: Int){
+        kmRan += km
+        println("Car has been driven $kmRan km")
+    }
+
+    fun applyBrake(){
+        println("Brakes applied successfully!")
+    }
+
+}
--- src/main/kotlin/ClassAndObject.kt
+++ src/main/kotlin/ClassAndObject.kt
@@ -1,3 +1,12 @@
 fun main(){
+
+    val mustang = Car("Ford","Petrol",1090)
+
+    println(mustang.name)
+    println( mustang.type)
+    println(mustang.kmRan)
+    mustang.driveCar(10)
+    mustang.applyBrake()
+
 }
 
