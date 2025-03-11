298452293f8534d635924999b82e202e46ab15e6
Rohit Singh <rohitrajput95547@gmail.com>
2025-03-11 21:01:58 +0530

    GS

--- src/main/kotlin/GetterAndSetter.kt
+++ src/main/kotlin/GetterAndSetter.kt
@@ -1,6 +1,7 @@
 fun main(){
 
-    val a1 = Aadmi("Marsh", 22)
+    val a1 = Aadmi("Marshal", 22)
+    a1.name = "marsh"
     a1.age = -10
 
 }
@@ -8,10 +9,12 @@ fun main(){
 class Aadmi(nameParam: String, ageParam: Int){
 
     var name: String = nameParam
+        set(value) {
+            if(value.length < 6) println("Empty") else field = value
+        }
     var age = ageParam
         set(value) {
-            if(value > 0) field = value else kotlin.io.println()
-            field = value
+            if(value > 0) field = value else println("Negative value can't be assigned")
         }
 
 }
