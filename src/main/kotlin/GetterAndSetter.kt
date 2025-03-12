d489bc4cd6f0862405d5b4fcbd01b78da868c474
Rohit Singh <rohitrajput95547@gmail.com>
2025-03-12 19:08:05 +0530

    GS final

--- src/main/kotlin/GetterAndSetter.kt
+++ src/main/kotlin/GetterAndSetter.kt
@@ -1,8 +1,11 @@
+import java.util.*
+
 fun main(){
 
-    val a1 = Aadmi("Marshal", 22)
-    a1.name = "marsh"
-    a1.age = -10
+    val a1 = Aadmi("Marshal", -22)
+    println( a1.name )
+    a1.name = "Marsh"
+    a1.age = -2
 
 }
 
@@ -10,11 +13,20 @@ class Aadmi(nameParam: String, ageParam: Int){
 
     var name: String = nameParam
         set(value) {
-            if(value.length < 6) println("Empty") else field = value
+            if(value.length < 6) println("Length should be greater than 6") else field = value
         }
+        get() {
+            return field.uppercase(Locale.getDefault())
+        }
+
     var age = ageParam
         set(value) {
             if(value > 0) field = value else println("Negative value can't be assigned")
         }
+
+    // By Default Kotlin does this
+    var email: String = ""
+        get() = field
+        set(value) { field = value }
 
 }
