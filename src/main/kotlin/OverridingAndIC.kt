bfd54ff657043fc90581589e38ef2ff5cd68dd83
Rohit Singh <rohitrajput95547@gmail.com>
2025-03-13 20:54:04 +0530

    Overriding, Parameterized classes and super

--- src/main/kotlin/OverridingAndIC.kt
+++ src/main/kotlin/OverridingAndIC.kt
@@ -1,11 +1,30 @@
 fun main(){
 
-    val d1 = Dog()
+    // Overriding
+    // Calling Parameterized classes in Inheritance context!!
+    // Calling Parent method using super keyword
+    // Every Kotlin class has (Any) as a Super class.
+
+    val d1 = Dog("Tiger")
     d1.eat("omnivores")
+    d1.run()
+    d1.animalInfo()
+
+    println( d1.toString() )
+
+    println()
+
+    val a1 = Animal("Roger")
+    a1.eat("Carnivores")
+    a1.run()
+    a1.animalInfo()
+
+    // Mentioned in Any class
+    println( a1.toString() )
 
 }
 
-open class Animal{
+open class Animal(val animalName: String){
 
     open val legs: Int = 4
     open val size: String = "s"
@@ -13,24 +32,31 @@ open class Animal{
     open val speed: Double = 45.0
 
     open fun eat(mode: String){
-        if(mode == modeOfNutrition) println("Eat Grass and trees")
-        else println("Eat any Animals!")
+        if(mode == modeOfNutrition) println("$animalName eat Grass and trees")
+        else println("$animalName eat any Animal!")
     }
 
-    open fun run() = println("Animal is running at a speed of $speed km/h")
+    open fun run() = println("$animalName is running at a speed of $speed km/h")
 
-    fun animalInfo() = println("$legs $size $modeOfNutrition $speed")
+    fun animalInfo() = println("$animalName have $legs legs is $size in size eats $modeOfNutrition food and runs at $speed km/h")
 
 }
 
-class Dog: Animal(){
+class Dog(typeParam: String): Animal(typeParam){
 
     override val modeOfNutrition: String = "omnivores"
 
-    override fun run() = println("MF Dog is running unnecessarily")
+    override fun run(){
+        super.run()
+        println("MF Dog is running unnecessarily")
+    }
 
     override fun eat(mode: String) {
-        if(mode == modeOfNutrition) println("Dog is Eating Dog Food")
+        if(mode == modeOfNutrition) println("$animalName is Eating Dog Food")
+    }
+
+    override fun toString(): String {
+        return animalInfo().toString()
     }
 
 }
