fcef6147d198786768f33294df7b73fa8bb1bc18
Rohit Singh <rohitrajput95547@gmail.com>
2025-03-19 15:18:22 +0530

    object keyword and expression

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -6,14 +6,12 @@ class Car(
 ){
 
     // methods of a class
-
     fun driveCar(km: Int){
         kmRan += km
         println("Car has been driven $kmRan km")
     }
 
-    fun applyBrake(){
-        println("Brakes applied successfully!")
-    }
+    fun applyBrake() = println("Brakes applied successfully!")
+
 
 }
