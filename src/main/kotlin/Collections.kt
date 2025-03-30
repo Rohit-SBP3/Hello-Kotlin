d2f34f25e2f884f9f9b7bbd7e842e8c11446a588
Rohit Singh <rohitrajput95547@gmail.com>
2025-03-30 18:53:04 +0530

    Collection Framework

---
+++ src/main/kotlin/Collections.kt
@@ -0,0 +1,19 @@
+
+/*** Kotlin's  Collection Framework...
+ *
+ * Immutable Collections:-
+ * List<T> - ordered collection with access to elements by indices
+ * Set<T> - collection of unique elements
+ * Map<K, V> - collection of key-value pairs
+ *
+ * Mutable Collections:-
+ * MutableList<T> - mutable version of List
+ * MutableSet<T> - mutable version of Set
+ * MutableMap<K, V> - mutable version of Map
+ *
+ * */
+
+
+fun main(){
+
+}
